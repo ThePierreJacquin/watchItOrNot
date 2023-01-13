4bc8a78ee8d26feb118141f5915120418a31ff4e
Pierre Jacquin <Pierre.jacquin@mindmatcher.org>
2023-01-13 21:48:34 +0100

    Fixed fonctions inputs and outputs

--- interface.py
+++ interface.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import os,sys
-# import cairosvg
+import cairosvg
 sys.path.insert(0, os.path.join(os.getcwd(),'src'))
 from watchOrNot import watchOrNot
-from apiUtils import getLastGame
+from apiUtils import getLastGame,getLink
 from time import time
-
+st.session_state.seuil = 5
 st.title("Should you watch yesterday Match ?")
 above = st.container()
 above.empty()
@@ -15,20 +15,20 @@ below.empty()
 
 def watchIt(game):
     below.write("Watch It !")
-    below.write(" ->link<- ")
+    below.write(getLink(game))
 
 def main():
     game = getLastGame(st.session_state.team)
     teams = list(game.keys())
-    # col1.image(cairosvg.svg2png(url='images/'+teams[0]+'.svg', output_width=426, output_height=240))
-    # col2.write(cairosvg.svg2png(url='images/VS.svg', output_width=426, output_height=240))
-    # col3.image(cairosvg.svg2png(url='images/'+teams[1]+'.svg', output_width=426, output_height=240))
+    col1.image(cairosvg.svg2png(url='images/'+teams[0]+'.svg', output_width=426, output_height=240))
+    col2.write(cairosvg.svg2png(url='images/VS.svg', output_width=426, output_height=240))
+    col3.image(cairosvg.svg2png(url='images/'+teams[1]+'.svg', output_width=426, output_height=240))
     
-    if watchOrNot(st.session_state.team):
+    if watchOrNot(game,st.session_state.team,st.session_state.seuil):
         watchIt(game)
     else:
         below.write("Don't waste your time, your team lost", unsafe_allow_html=True)
-    
+
 teamTricodes = ["---","ATL","BKN","BOS","CHA","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOP","NYK","OKC","ORL","PHI","PHX","POR","SAC","SAS","TOR","UTA","WAS"]
 above.selectbox("Select your team abbreviation",teamTricodes,key="team",on_change=main)
     
--- watchOrNot.py
+++ watchOrNot.py
@@ -4,11 +4,15 @@ sys.path.insert(0, os.path.join(os.getcwd(),'src'))
 from apiUtils import getLastGame
 
 
-def watchOrNot(team:str):
-    game = getLastGame(team)
-    scoreF = game[team]
-    scoreA = game[(game.keys()-{team}).pop()] 
-    if scoreA-10 < scoreF: ####faire le traitement dès le début
+def watchOrNot(boxscore:dict,team:str,seuil):
+    teams = list(boxscore.keys())
+    if team == teams[0]:
+        scoreF = boxscore[teams[0]]
+        scoreA = boxscore[teams[1]] 
+    else:
+        scoreA = boxscore[teams[0]]
+        scoreF = boxscore[teams[1]] 
+    if scoreA-seuil < scoreF: 
         return True
     return False
 
