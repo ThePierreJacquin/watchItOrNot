03d477e9c2f10d7009de51000fcb12053f89ebec
Pierre Jacquin <Pierre.jacquin@mindmatcher.org>
2023-01-27 20:35:13 +0100

    Version 1.0

--- interface.py
+++ interface.py
@@ -2,13 +2,13 @@ import streamlit as st
 import os,sys
 import streamlit as st
 import base64
-import textwrap
 sys.path.insert(0, os.path.join(os.getcwd(),'src'))
 from watchOrNot import watchOrNot
 from apiUtils import getLastGame,getLink
-from time import time
-st.session_state.seuil = 5
-st.title("Should you watch yesterday Match ?")
+
+if "seuil" not in st.session_state:
+    st.session_state.seuil = 10
+st.title("Should I watch yesterday's match ?")
 above = st.container()
 above.empty()
 col1,col2,col3 = st.columns([1,1,1])
@@ -17,7 +17,7 @@ below.empty()
 
 st.markdown("""
 <style>
-.css-1offfwp p {
+p {
     font-size:20px !important;
     font-weight: bold;
     text-align: center;
@@ -50,7 +50,15 @@ def main():
             watchIt(game)
         else:
             below.write("Don't waste your time, your team lost")
-
-teamTricodes = ["---","ATL","BKN","BOS","CHA","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOP","NYK","OKC","ORL","PHI","PHX","POR","SAC","SAS","TOR","UTA","WAS"]
-above.selectbox("Select your team abbreviation",teamTricodes,key="team",on_change=main)
+with above:
+    btMns,text,btPs = st.columns([1,10,1])
+    
+    if btMns.button(":heavy_minus_sign:"):
+        st.session_state.seuil -=1
+    if btPs.button(":heavy_plus_sign:"):
+        st.session_state.seuil +=1
+    text.write("'I don't want to watch if my team lost by more than " + str(st.session_state.seuil) + " points'")
+    
+    teamTricodes = ["---","ATL","BKN","BOS","CHA","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOP","NYK","OKC","ORL","PHI","PHX","POR","SAC","SAS","TOR","UTA","WAS"]
+    above.selectbox("Select your team abbreviation",teamTricodes,key="team",on_change=main,label_visibility="collapsed")
     
