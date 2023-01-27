4c39158679deb3cf832859853736284e29bdd047
Pierre Jacquin <Pierre.jacquin@mindmatcher.org>
2023-01-27 19:59:38 +0100

    Fixed svg support, added css for text

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os,sys
-import cairosvg
+import streamlit as st
+import base64
+import textwrap
 sys.path.insert(0, os.path.join(os.getcwd(),'src'))
 from watchOrNot import watchOrNot
 from apiUtils import getLastGame,getLink
@@ -13,21 +15,41 @@ col1,col2,col3 = st.columns([1,1,1])
 below = st.container()
 below.empty()
 
+st.markdown("""
+<style>
+.css-1offfwp p {
+    font-size:20px !important;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
+def render_svg(svg):
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
+
 def watchIt(game):
     below.write("Watch It !")
     below.write(getLink(game))
 
 def main():
-    game = getLastGame(st.session_state.team)
-    teams = list(game.keys())
-    col1.image(cairosvg.svg2png(url='images/'+teams[0]+'.svg', output_width=426, output_height=240))
-    col2.write(cairosvg.svg2png(url='images/VS.svg', output_width=426, output_height=240))
-    col3.image(cairosvg.svg2png(url='images/'+teams[1]+'.svg', output_width=426, output_height=240))
-    
-    if watchOrNot(game,st.session_state.team,st.session_state.seuil):
-        watchIt(game)
-    else:
-        below.write("Don't waste your time, your team lost", unsafe_allow_html=True)
+    if st.session_state.team != "---":
+        game = getLastGame(st.session_state.team)
+        teams = list(game.keys())
+        with col1:
+            render_svg(open("images/"+teams[0]+".svg").read())
+        with col2:
+            render_svg(open('images/VS.svg').read())
+        with col3:
+            render_svg(open("images/"+teams[1]+".svg").read())
+        
+        if watchOrNot(game,st.session_state.team,st.session_state.seuil):
+            watchIt(game)
+        else:
+            below.write("Don't waste your time, your team lost")
 
 teamTricodes = ["---","ATL","BKN","BOS","CHA","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOP","NYK","OKC","ORL","PHI","PHX","POR","SAC","SAS","TOR","UTA","WAS"]
 above.selectbox("Select your team abbreviation",teamTricodes,key="team",on_change=main)
